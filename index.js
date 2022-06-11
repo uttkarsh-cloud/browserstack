b406cf6a79e2aee33877bdfd4c385904ae99504d
Utkarsh Bajpai <ubajpai28@gmail.com>
2022-06-11 12:39:27 +0530

    Few changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3000;
 
-// var db = new sqlite3.Database('C:/Users/Utkarsh/OneDrive/Desktop/History');
-// console.log(db);
+ var db = new sqlite3.Database('C:/Users/Utkarsh/OneDrive/Desktop/History');
+ console.log(db);
 
 app.get('/start/:browser/:url', async (req, res) => {
     const browser = req.params.browser;
@@ -30,11 +30,11 @@ function getCurrentUrl(req){
 }
 
 app.get('/history', async (req, res) => {
-    // db.serialize(function () {
-    //     db.all('SELECT * FROM urls ORDER BY id DESC LIMIT 10;', function (err, rows) {
-    //         console.log(rows);
-    //     });
-    // });
+     db.serialize(function () {
+        db.all('SELECT * FROM urls ORDER BY id DESC LIMIT 10;', function (err, rows) {
+             console.log(rows);
+         });
+     });
 
 });
 
@@ -52,8 +52,8 @@ app.get('/delete/:browser', async (req, res) => {
 });
 
 app.get('/geturl/:browser', async (req, res) => {
-    // const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-    // console.log("Current tab url",fullUrl)
+     const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+     console.log("Current tab url",fullUrl)
 });
 
 
